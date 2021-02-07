54b988eb9c40bf79c70b6e94739397b69afea08d
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-02-08 01:47:29 +0600

    someCodeModification

--- java.js
+++ java.js
@@ -1,57 +1,76 @@
-function clickResponse() {
-  let inputText = document.getElementById("input-text").value;
+const clickResponse = () => {
+  const inputText = document.getElementById("input-text").value;
   if (inputText.length > 1) {
     alert("Input must be a single Character");
     document.getElementById("input-text").value = "";
   } else {
-    let imageDiv = document.getElementById("image");
-    let showingIngredients = document.getElementById("showing-ingredients");
+    const showingIngredients = document.getElementById("showing-ingredients");
     showingIngredients.innerHTML = "";
-    imageDiv.innerHTML = "";
+
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputText}`)
       .then((res) => res.json())
-      .then((data) => {
-        let mealsName = data.meals;
-        mealsName
-          .forEach((meal) => {
-            let imageDetails = document.createElement("div");
-            imageDetails.className = "image-div-details";
-            let mealId = meal.idMeal;
-            imageDetails.setAttribute("onclick", `mealDetails(${mealId})`);
-            imageDetails.innerHTML = `
+      .then((data) => foodFinder(data))
+      .catch((error) => errorMessage(error));
+  }
+};
+
+const foodFinder = (data) => {
+  errorMessageClear();
+  const imageDiv = document.getElementById("image");
+  imageDiv.innerHTML = "";
+  const mealsName = data.meals;
+  mealsName.forEach((meal) => {
+    const imageDetails = document.createElement("div");
+    imageDetails.className = "image-div-details";
+    const mealId = meal.idMeal;
+    imageDetails.setAttribute("onclick", `mealDetails(${mealId})`);
+    imageDetails.innerHTML = `
             <img class="image-size" src=${meal.strMealThumb}>
             <h2 class="text-center">${meal.strMeal}</h2>
             `;
-            imageDiv.appendChild(imageDetails);
-            document.getElementById("input-text").value = "";
-          })
-          .catch((error) => console.log(error));
-      });
-  }
-}
-function mealDetails(mealIdValue) {
-  let showingIngredients = document.getElementById("showing-ingredients");
+    imageDiv.appendChild(imageDetails);
+    document.getElementById("input-text").value = "";
+  });
+};
+
+const mealDetails = (mealIdValue) => {
   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealIdValue}`)
     .then((res) => res.json())
-    .then((data) => {
-      showingIngredients.innerHTML = "";
-      let heading = document.createElement("h1");
-      heading.innerText = `Ingredients`;
-      showingIngredients.appendChild(heading);
-      let requiredObject = data.meals[0];
-      let objectKeys = Object.keys(requiredObject);
-      let objectKeysLength = objectKeys.length;
-      objectKeys.forEach((everyObject) => {
-        for (let i = 0; i < objectKeysLength; i++) {
-          if (everyObject === `strIngredient${[i]}`) {
-            let myObj = `${requiredObject[everyObject]}`;
-            if (myObj != "" && myObj != "null") {
-              let li = document.createElement("li");
-              li.innerText = `${myObj}`;
-              showingIngredients.appendChild(li);
-            }
-          }
+    .then((data) => ingredientHandler(data));
+};
+const ingredientHandler = (data) => {
+  const foodImage = document.getElementById("food-image");
+  const showingIngredients = document.getElementById("showing-ingredients");
+  showingIngredients.innerHTML = "";
+  const requiredObject = data.meals[0];
+  const heading = document.createElement("h1");
+  heading.className = "text-center";
+  foodImage.innerHTML = `<img class="img-fluid img-thumbnail mt-3" src="${requiredObject.strMealThumb}"> `;
+  heading.innerText = `Ingredients of ${requiredObject.strMeal}`;
+  showingIngredients.appendChild(heading);
+  const objectKeys = Object.keys(requiredObject);
+  const objectKeysLength = objectKeys.length;
+  objectKeys.forEach((everyObject) => {
+    for (let i = 0; i < objectKeysLength; i++) {
+      if (everyObject === `strIngredient${[i]}`) {
+        const myObj = `${requiredObject[everyObject]}`;
+        if (myObj != "" && myObj != "null") {
+          const li = document.createElement("li");
+          li.innerText = `${myObj}`;
+          showingIngredients.appendChild(li);
         }
-      });
-    });
-}
+      }
+    }
+  });
+};
+const errorMessage = (error) => {
+  const errorMessageGiver = document.getElementById("error-message-giver");
+  const errorMessageContext = document.createElement("H2");
+  errorMessageContext.innerText = `Something went wrong. Please try again`;
+  errorMessageGiver.appendChild(errorMessageContext);
+};
+const errorMessageClear = () => {
+  const errorMessageGiver = (document.getElementById(
+    "error-message-giver"
+  ).innerHTML = "");
+};
